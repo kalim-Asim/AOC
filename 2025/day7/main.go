f76c7c416e840db3d3a7ee71c105e480389b5e92
kalim-Asim <asim1306iiit@gmail.com>
2025-12-12 10:28:31 +0530

    part2: memoization worked

--- 2025/day7/main.go
+++ 2025/day7/main.go
@@ -22,7 +22,8 @@ func main() {
 	rows, cols = len(grid), len(grid[0])
 
 
-	part1(grid)
+	// part1(grid)
+	part2(grid)
 }
 
 func part1(grid []string) {
@@ -82,4 +83,52 @@ func part1(grid []string) {
 		}
 	}
 	fmt.Println("Part1: ", split.Size())
+}
+
+
+func part2(grid []string) {
+	var start utils.Pair[int, int]
+	for col := 0; col < cols; col++ {
+		if grid[0][col] == 'S' {
+			start = utils.Pair[int, int]{X: 0, Y: col}
+		}
+	}
+
+	valid := func(x, y int) bool {
+		return x >= 0 && y >= 0 && x < rows && y < cols 
+	}
+
+	// recursion which will go L/R till it reaches last row
+	// you must define function outside to use as recursion inside
+	var f func(int, int) int
+	dp := make([][]int, rows)
+	for i := range dp {
+		dp[i] = make([]int, cols)
+		for j := range dp[i] {
+			dp[i][j] = -1
+		}
+	}
+	f = func(x, y int) int {
+		if !valid(x, y) {
+			return 0
+		}
+		if dp[x][y] != -1 {
+			return dp[x][y]
+		}
+		// down
+		nx, ny := x + 1, y 
+		if nx == rows {
+			return 1
+		}
+
+		if grid[nx][ny] == '^' {
+			// split L + R
+			dp[x][y] = f(nx, ny - 1) + f(nx, ny + 1)
+			return dp[x][y]
+		} 
+		dp[x][y] = f(nx, ny)
+		return f(nx, ny)
+	}
+
+	fmt.Println("Part2: ", f(start.X, start.Y))
 }
